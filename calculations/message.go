d5061183203954ca2aee418dbe2fc53f3e8eea25
Leonardo Graterol <pankas87@gmail.com>
2022-02-22 14:18:09 -0300

    Progress backup

--- calculations/message.go
+++ calculations/message.go
@@ -13,5 +13,5 @@ func mergeMessages(m1, m2 []string) {
 	//			- Posición en Slice 1: 5
 	//			- Posición en Slice 2: 3
 	//	- Inicializar un puntero en "posición 0" para recorrer ambos slices
-	//		- Calculo de "posición 0": Pendiente de pensar la lógica
+	//	- El puntero para el array donde la palabra en común esté más adelante, se calcula en base a un offset que es la diferencia entre la posición mayor y la menor
 }
